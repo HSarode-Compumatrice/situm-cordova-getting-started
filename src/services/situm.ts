98148a9053e0ad1e737aa8cbe27ad47697f923a1
HSarode-Compumatrice <harshal@compumatrice.com>
2018-07-27 17:04:19 +0530

    first situm getting start commit

--- src/services/situm.ts
+++ src/services/situm.ts
@@ -1,2 +1,2 @@
-export const USER_EMAIL = 'USER_EMAIL';
-export const USER_API_KEY = 'YOUR_API_KEY';
+export const USER_EMAIL = 'shrikrishna@compumatrice.com';
+export const USER_API_KEY = '6e6314eb606a6d31da98cd0df865e327d3cb13d1eacbc509d69bbc996607d0e4';
